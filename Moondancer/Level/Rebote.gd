7222709fa28485c6b8d328209859a9b04f3f064b
Jose26398 <jsg26398@gmail.com>
2020-09-05 22:14:14 +0200

    Fixed detection bug

--- Moondancer/Level/Rebote.gd
+++ Moondancer/Level/Rebote.gd
@@ -2,14 +2,15 @@ extends Area2D
 
 var is_alive = false
 
-func _on_Rebote1_body_entered(body):
-	if body is Player and Input.is_action_pressed("ui_down"):
-		is_alive = true
-
-
 func _on_Rebote1_body_exited(body):
 	if is_alive:
 		is_alive = false
 		print("Esta vivo. Animacion de agua.")
 	else:
 		print("Esta muerto. Animacion de muerte.")
+
+
+func _process(delta):
+	if get_overlapping_bodies().size() == 2 and Input.is_action_pressed("ui_down"):
+		is_alive = true
+		print(get_overlapping_bodies().size())
